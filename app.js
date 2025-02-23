1e133b3d97d9291ceb9fb2624e6b0093c532c67b
Hustler <muhammad2157.be23@chitkara.edu.in>
2025-02-23 20:20:02 +0530

    frontend commit

--- app.js
+++ app.js
@@ -65,5 +65,5 @@ app.post("/add-task", (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}/add-task`);
 });
